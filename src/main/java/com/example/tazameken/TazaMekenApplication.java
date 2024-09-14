f3921dc3e62aba60761767db05479d6c9bce23e8
Ashimov Argen <retreatpm@icloud.com>
2024-09-14 07:48:12 +0300

    Start

---
+++ src/main/java/com/example/tazameken/TazaMekenApplication.java
@@ -0,0 +1,13 @@
+package com.example.tazameken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TazaMekenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TazaMekenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tazameken/TazaMekenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tazameken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TazaMekenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
