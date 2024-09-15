79f3b5159e82ba815dc115592d3988097fd286a9
Ashimov Argen <retreatpm@icloud.com>
2024-09-15 12:38:00 +0300

    add more clothes

---
+++ src/main/resources/db/changelog/initial/008_insert_more_clothes.sql
@@ -0,0 +1,8 @@
+INSERT INTO clothes(category_id, color_id, gender_id, size, price, photo_name, is_sold)
+VALUES
+    (1, 1, 1, 'L', 1300, 'redtshirt.jpg', false),    -- Футболка, Красный, Мужской
+    (1, 4, 2, 'M', 950, 'bluetshirt.jpg', false),    -- Футболка, Синий, Женский
+    (1, 6, 3, 'S', 1100, 'greentshirt.jpg', false),  -- Футболка, Зеленый, Унисекс
+    (2, 2, 1, 'S', 750, 'greycap.jpg', false),       -- Кепка, Серый, Мужской
+    (3, 1, 2, 'M', 2700, 'redsweater.jpg', true),    -- Свитер, Красный, Женский
+    (3, 4, 1, 'L', 2600, 'bluesweater.jpg', false);  -- Свитер, Синий, Мужской
